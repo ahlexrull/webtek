bbc8b312e2ecec0bd3f58d7dfb46a15295c20d4c
popsbear <bigoreuel.rb@gmail.com>
2017-05-06 02:09:22 +0800

    initial pages for new-feedback.php and old-feedback.php

--- pages/old-feedback.php
+++ pages/old-feedback.php
@@ -5,12 +5,17 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Old Feedback</title><meta charset="UTF-8" />
-
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">  
-    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
-    <script type="text/javascript" scr="assets/js/script.js" defer="defer"></script>
-    
+    <!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+     <!-- MORRIS CHART STYLES-->
+    <link href="assets/js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+     <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+    <!-- TABLE STYLES-->
+    <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+    <script src="assets/js/script.js" defer="defer"></script>
 </head>
 <body>
     
@@ -41,44 +46,57 @@
             <div id="page-inner">
                 <div class="row">
                     <div class="col-md-12">
-                        <h2>FeedBack</h2>   
+                        <h2>Old FeedBack</h2>   
                     </div>    
                 </div>
-                <div class="jumbotron">
-                   <?php
-                        $query = $pdo->prepare("SELECT * FROM feedback where feedback_status = 0 ");
-                        $query->execute();
-                        $result = $query->fetchAll(); 
-                    ?>
-                    
-                    <button type="button" class="btn btn-default" id="reply_btn">Reply</button>
-                    <button type="button" class="btn btn-primary">Done</button>
+               <?php
+                    $query = $pdo->prepare("select f.ranking, f.contacting_phone_number cpn, f.feedback_messages m, u.username, concat(u.first_name, ' ', u.middle_name, ' ', u.last_name) name, u.email_address ea, f.feedback_date fd from feedback f natural join user_account u where f.feedback_status = 0;");
+                    $query->execute();
 
-                    
-                </div>
+                    while ($row = $query->fetch(PDO::FETCH_ASSOC)){
+                        $ranking = $row['ranking'];
+                        $cpn = $row['cpn'];
+                        $message = $row['m'];
+                        $usr = $row['username'];
+                        $name = $row['name'];
+                        $email = $row['ea'];
+                        $fdate = $row['fd'];
+                        
+                        echo ('<div class="jumbotron">'
+                            .$ranking
+                            .$cpn
+                            .$message
+                            .$usr
+                            .$name
+                            .$email
+                            .$fdate.
+                            '<button id="reply_btn" type="button" class="btn btn-default">Reply</button>'.
+                            '<button type="button" class="btn btn-primary">Done</button>'.
+                        '</div>');
+                    }
 
+                ?>
                               
             </div>
         </div>
-        <div id="reply_modal" class="modal fade" role="dialog">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title">Modal title</h4>
-                    </div>
-                    <div class="modal-body">
-                        <p>One fine body…</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-primary">Save Reply</button>
-                    </div>
+    </div>
+    
+    <div id="reply_modal" class="modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                    <p>One fine body…</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary">Save Reply</button>
                 </div>
             </div>
         </div>
     </div>
-    <script>
-    </script>
 </body>
 </html>    
